7b584c603955f1d1b2eb301951ce11ec73797967
李斌 <2414592137@qq.com>
2023-06-07 16:56:53 +0800

    修改nacos主类名

--- spring-cloud-nacos/src/main/java/com/example/childrendemo/SpringCloudNacosApplication.java
+++ spring-cloud-nacos/src/main/java/com/example/childrendemo/SpringCloudNacosApplication.java
@@ -6,11 +6,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ChildrenApplication {
+public class SpringCloudNacosApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(ChildrenApplication.class, args);
+        SpringApplication.run(SpringCloudNacosApplication.class, args);
     }
 
 }
