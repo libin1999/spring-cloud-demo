82462fdd2ca979b5c0122bd252a53abfd1406a8a
李斌 <2414592137@qq.com>
2023-06-07 16:51:19 +0800

    提交测试

--- spring-cloud-nacos/src/main/java/com/example/childrendemo/ChildrenApplication.java
+++ spring-cloud-nacos/src/main/java/com/example/childrendemo/ChildrenApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ChildrenApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ChildrenApplication.class, args);
     }
 
